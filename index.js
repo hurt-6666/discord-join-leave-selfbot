d94512a105bb38ac477a4fc65f3e273631fbead5
hurt#6666 on discord <btdmods2015@gmail.com>
2019-08-12 18:12:18 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const d = require('discord.js')
 const bot = new d.Client()
 const request = require('request')
 bot.on('message',async msg => {
-    if(msg.author.id !== 'your id') return
+    if(msg.author.id !== '489219473476157460') return
     if(msg.channel.type !== 'dm') return
     if(msg.content.toLowerCase().startsWith('^leave')){
         let args = msg.content.split(" ").slice(1)
-        if(!args) msg.channel.send(`Please provide an invite like of the server to leave!`)
-        if(args[0].length > 18) return msg.channel.send(`That is now a valid invite link!`)
+        if(!args) msg.channel.send(`Please provide a guild ID of the server to leave!`)
+        if(args[0].length > 18) return msg.channel.send(`That is now a valid guild ID!`)
         var request = require("request");
 
         var options = { method: 'DELETE',
@@ -17,14 +17,16 @@ bot.on('message',async msg => {
          { 
            Host: 'discordapp.com',
            Accept: '*/*',
-           Authorization: String('token').replace('\r',''),
+           Authorization: String('NjEwNTkyOTE0NzQ1MDY1NTA3.XVHhJA.kRkPqqobXvX35EZSvYqOcNkz8Nc').replace('\r',''),
            'Content-Type': 'application/json' },
         json: true };
     request(options, function (error, response, body) {
     if (error) return msg.channel.send(error)
+    // msg.channel.send(`Joined server ${body}`)
     console.log(response.body)
     if(response.body === undefined) return msg.channel.send(`Successfully left the server \`${bot.guilds.get(args[0])}\``)
     if(response.body.message) return msg.channel.send(response.body.message)
+    // console.log(body);
     });
     }
     if(msg.content.toLowerCase().startsWith('^join')){
@@ -41,14 +43,17 @@ var options = { method: 'POST',
      Connection: 'keep-alive',
      Host: 'discordapp.com',
      Accept: '*/*',
-     Authorization: String('token').replace('\r',''),
+     Authorization: String('NjEwNTkyOTE0NzQ1MDY1NTA3.XVHhJA.kRkPqqobXvX35EZSvYqOcNkz8Nc').replace('\r',''),
      'Content-Type': 'application/json' },
     json: true };
     request(options, function (error, response, body) {
     if (error) return msg.channel.send(error)
     if(body.message === 'The user is banned from this guild.') return msg.channel.send(body.message)
+    // msg.channel.send(`Joined server ${body}`)
+    // console.log(body.guild.name)
     msg.channel.send(`Successfully joined the server \`${body.guild.name}\``)
+    // console.log(body);
     });
     }
 })
-bot.login(`token`)
+bot.login(`NjEwNTkyOTE0NzQ1MDY1NTA3.XVHhJA.kRkPqqobXvX35EZSvYqOcNkz8Nc`)
--- readme.md
+++ readme.md
@@ -3,3 +3,5 @@ This is just a simple few lines of code to use the discord api to join / leave s
 Use this at your own risk discord has brain cells and can see if you do this too many times (ban basically)
 
 To get around a ban use proxies :)
+
+note: this is setup to just work with the last 6 letters of the invite like! use a guild id to leave a guild!
